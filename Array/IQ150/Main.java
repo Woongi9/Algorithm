745a54bb4e8dea65476a420af119c205eb8ad83a
Woongi9 <79649052+Woongi9@users.noreply.github.com>
2022-02-24 11:43:34 +0900

    Create Main.java

---
+++ Array/IQ150/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		if(n<1 || n>20) {
+		}
+		else
+		{
+			int arr[][]; 
+			arr = new int [n][n];
+		
+			for(int i = 0; i<n; i++)
+				arr[i][0] = sc.nextInt();
+			
+			for(int i = 0; i < n; i++)
+			{
+				for(int j = 1; j < i+1; j++)
+				{
+					arr[i][j] = arr[i][j-1] - arr[i-1][j-1];
+				}
+			}
+		
+			for(int i = 0; i<n; i++)
+			{
+				for(int j = 0; j<i+1; j++)
+				{
+					System.out.printf(+arr[i][j]+ " ");
+				}
+				System.out.println("");
+			}
+		}
+		
+	}
+
+}
