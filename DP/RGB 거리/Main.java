347d0a017773407be067c3cbc8e6ddf4e3460903
Woongi9 <79649052+Woongi9@users.noreply.github.com>
2022-04-22 23:48:14 +0900

    Create Main.java

---
+++ DP/RGB 거리/Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, prev, min;
+	static int [][] cost;
+	static int [] sum;
+	static int [] choice;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		
+		cost = new int [n][3];
+		sum = new int [n];
+		choice = new int [n];
+		
+		for(int i = 0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for(int j = 0; j<3; j++) {
+				cost[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 1; i < n; i++) {
+			cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
+			cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
+			cost[i][2] += Math.min(cost[i - 1][0], cost[i - 1][1]);
+		}
+		
+		System.out.print(Math.min(cost[n-1][0], Math.min(cost[n-1][1], cost[n-1][2])));
+		
+	}
+
+}
